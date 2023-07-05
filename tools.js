316a171eac619e4c5055d12a569e81af19d24070
Sameer Suman <sameersuman2403@gmail.com>
2023-07-05 13:18:39 +0530

    implemented upload tool functionalities

--- tools.js
+++ tools.js
@@ -1,94 +1,133 @@
-let toolsConatiner = document.querySelector(".tools-container");
+let toolContainer = document.querySelector(".tools-container");
 let optionsContainer = document.querySelector(".options-container");
 let optionsFlag = true;
 let pencilToolContainer = document.querySelector(".pencil-tool-container");
 let eraserToolContainer = document.querySelector(".eraser-tool-container");
 let pencil = document.querySelector(".pencil");
 let eraser = document.querySelector(".eraser");
+let sticky = document.querySelector(".sticky");
+let upload = document.querySelector(".upload");
 let pencilFlag = false;
 let eraserFlag = false;
-let sticky = document.querySelector(".sticky");
 
-// true -> show tools, false -> hide tools
 optionsContainer.addEventListener("click", (e) => {
+  // true -> tools show, false -> hide tools
   optionsFlag = !optionsFlag;
-  if (optionsFlag) showTools();
-  else hideTools();
+
+  if (optionsFlag) openTools();
+  else closeTools();
 });
 
-function showTools() {
-  let iconEle = optionsContainer.children[0];
-  iconEle.classList.remove("fa-times");
-  iconEle.classList.add("fa-bars");
-  toolsConatiner.style.display = "flex";
+function openTools() {
+  let iconElem = optionsContainer.children[0];
+  iconElem.classList.remove("fa-times");
+  iconElem.classList.add("fa-bars");
+  toolContainer.style.display = "flex";
 }
+function closeTools() {
+  let iconElem = optionsContainer.children[0];
+  iconElem.classList.remove("fa-bars");
+  iconElem.classList.add("fa-times");
+  toolContainer.style.display = "none";
 
-function hideTools() {
-  let iconEle = optionsContainer.children[0];
-  iconEle.classList.remove("fa-bars");
-  iconEle.classList.add("fa-times");
-  toolsConatiner.style.display = "none";
-
-  // reset all tool selected status to default
   pencilToolContainer.style.display = "none";
   eraserToolContainer.style.display = "none";
 }
 
-// pencil tool
 pencil.addEventListener("click", (e) => {
+  // true -> show pencil tool, false -> hide pencil tool
   pencilFlag = !pencilFlag;
-  if (pencilFlag) {
-    pencilToolContainer.style.display = "block";
-  } else {
-    pencilToolContainer.style.display = "none";
-  }
+
+  if (pencilFlag) pencilToolContainer.style.display = "block";
+  else pencilToolContainer.style.display = "none";
 });
 
-// eraser tool
 eraser.addEventListener("click", (e) => {
+  // true -> show eraser tool, false -> hide eraser tool
   eraserFlag = !eraserFlag;
-  if (eraserFlag) {
-    eraserToolContainer.style.display = "flex";
-  } else {
-    eraserToolContainer.style.display = "none";
-  }
+
+  if (eraserFlag) eraserToolContainer.style.display = "flex";
+  else eraserToolContainer.style.display = "none";
 });
 
-// sticky tool
-sticky.addEventListener("click", (e) => {
-  let stickyContainer = document.createElement("div");
-  stickyContainer.setAttribute("class", "sticky-container");
-  stickyContainer.innerHTML = `
+upload.addEventListener("click", (e) => {
+  // Open file explorer
+  let input = document.createElement("input");
+  input.setAttribute("type", "file");
+  input.click();
+
+  input.addEventListener("change", (e) => {
+    let file = input.files[0];
+    let url = URL.createObjectURL(file);
+
+    let stickyTemplateHTML = `
         <div class="header-container">
             <div class="minimize"></div>
             <div class="remove"></div>
-      </div>
-      <div class="note-container">
-        <textarea></textarea>
-      </div>
+        </div>
+        <div class="note-container">
+            <img src="${url}"/>
+        </div>
+        `;
+    createSticky(stickyTemplateHTML);
+  });
+});
+
+sticky.addEventListener("click", (e) => {
+  let stickyTemplateHTML = `
+    <div class="header-container">
+        <div class="minimize"></div>
+        <div class="remove"></div>
+    </div>
+    <div class="note-container">
+        <textarea spellcheck="false"></textarea>
+    </div>
     `;
+
+  createSticky(stickyTemplateHTML);
+});
+
+function createSticky(stickyTemplateHTML) {
+  let stickyContainer = document.createElement("div");
+  stickyContainer.setAttribute("class", "sticky-container");
+  stickyContainer.innerHTML = stickyTemplateHTML;
   document.body.appendChild(stickyContainer);
 
+  let minimize = stickyContainer.querySelector(".minimize");
+  let remove = stickyContainer.querySelector(".remove");
+  noteActions(minimize, remove, stickyContainer);
+
   stickyContainer.onmousedown = function (event) {
-    dragNDrop(stickyContainer, event);
+    dragAndDrop(stickyContainer, event);
   };
 
   stickyContainer.ondragstart = function () {
     return false;
   };
-});
+}
 
-function dragNDrop(element, event) {
+function noteActions(minimize, remove, stickyContainer) {
+  remove.addEventListener("click", (e) => {
+    stickyContainer.remove();
+  });
+  minimize.addEventListener("click", (e) => {
+    let noteContainer = stickyContainer.querySelector(".note-container");
+    let display = getComputedStyle(noteContainer).getPropertyValue("display");
+    if (display === "none") noteContainer.style.display = "block";
+    else noteContainer.style.display = "none";
+  });
+}
+
+function dragAndDrop(element, event) {
   let shiftX = event.clientX - element.getBoundingClientRect().left;
   let shiftY = event.clientY - element.getBoundingClientRect().top;
 
   element.style.position = "absolute";
   element.style.zIndex = 1000;
-  // document.body.append(element);
 
   moveAt(event.pageX, event.pageY);
 
-  // moves the element at (pageX, pageY) coordinates
+  // moves the sticky-note at (pageX, pageY) coordinates
   // taking initial shifts into account
   function moveAt(pageX, pageY) {
     element.style.left = pageX - shiftX + "px";
@@ -99,10 +138,10 @@ function dragNDrop(element, event) {
     moveAt(event.pageX, event.pageY);
   }
 
-  // move the element on mousemove
+  // move the sticky-note on mousemove
   document.addEventListener("mousemove", onMouseMove);
 
-  // drop the element, remove unneeded handlers
+  // drop the sticky-note, remove unneeded handlers
   element.onmouseup = function () {
     document.removeEventListener("mousemove", onMouseMove);
     element.onmouseup = null;
